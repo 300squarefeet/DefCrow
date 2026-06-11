3e696acb41b830afafa0e6d1ae78cb320f9a95f9
xsnizer <23221101@std.stei.itb.ac.id>
2026-06-11 14:54:17 +0800

    feat: add /settings route to App router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './store/auth'
 import ProtectedRoute from './components/ProtectedRoute'
-import LoginPage     from './pages/LoginPage'
-import GeneratorPage from './pages/GeneratorPage'
-import JobStatusPage from './pages/JobStatusPage'
+import LoginPage      from './pages/LoginPage'
+import GeneratorPage  from './pages/GeneratorPage'
+import JobStatusPage  from './pages/JobStatusPage'
+import SettingsPage   from './pages/SettingsPage'
 
 export default function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<ProtectedRoute><GeneratorPage /></ProtectedRoute>} />
-        <Route path="/job/:id" element={<ProtectedRoute><JobStatusPage /></ProtectedRoute>} />
+        <Route path="/login"    element={<LoginPage />} />
+        <Route path="/"         element={<ProtectedRoute><GeneratorPage /></ProtectedRoute>} />
+        <Route path="/job/:id"  element={<ProtectedRoute><JobStatusPage /></ProtectedRoute>} />
+        <Route path="/settings" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
       </Routes>
     </AuthProvider>
   )
